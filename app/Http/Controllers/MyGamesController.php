cd3f909e9dd695c4e13f2d2b7b541626f252eeb6
Genesis Ivan Ilagan <ivanilagan1109@gmail.com>
2025-01-03 13:11:32 +0800

    feat: Create My Games List API

--- app/Http/Controllers/MyGamesController.php
+++ app/Http/Controllers/MyGamesController.php
@@ -3,12 +3,27 @@
 namespace App\Http\Controllers;
 
 use App\Services\MyGamesService;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 
 class MyGamesController extends Controller
 {
+    public function index(Request $request): JsonResponse
+    {
+
+        $service = (new MyGamesService())->get_my_games(Auth::user()->id);
+
+        if ($service->successful) {
+            $resp = response()->json($service->result);
+        } else {
+            $resp = response()->json($service->error, 422);
+        }
+
+        return $resp;
+    }
+
     public function store(Request $request)
     {
         $validator = Validator::make($request->all(), [
